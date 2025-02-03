ddb103ad7d00676938a9051920ac5a1408404e4c
Fakhry <fakhrymubarak@gmail.com>
2025-02-03 23:15:13 +0700

    change icon

--- lib/pages/wish/wish_page.dart
+++ lib/pages/wish/wish_page.dart
@@ -3,6 +3,8 @@ import 'package:gif/gif.dart';
 import 'package:shafa_25_birthday/pages/wish/components/wish_widget.dart';
 import 'package:shafa_25_birthday/themes/themes.dart';
 
+import 'components/confetti_wish_wrapper.dart';
+
 class WishPage extends StatelessWidget {
   const WishPage({super.key});
 
@@ -15,43 +17,45 @@ class WishPage extends StatelessWidget {
     final double margin = screenHeight * 0.075;
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: margin),
-            Text(
-              "My Dearest Shafaaaa ⸜(｡˃ ᵕ ˂ )⸝♡",
-              style: isMobile
-                  ? TextStyles.extraBold28ptStix()
-                  : TextStyles.extraBold60ptStix(),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: margin),
-            Expanded(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: screenWidth * 0.9),
-                child: Row(
-                  children: [
-                    Align(
-                        alignment: Alignment.topRight,
-                        child: _buildGif(Assets.gifCatFlower2, screenWidth * 0.1)),
-                    Expanded(child: WishWidget()),
-                    Align(
-                        alignment: Alignment.bottomLeft,
-                        child: _buildGif(Assets.gifCatFlower, screenWidth * 0.1)),
-                  ],
+        child: ConfettiWishWrapper(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: margin),
+              Text(
+                "My Dearest Shafaaaa ⸜(｡˃ ᵕ ˂ )⸝♡",
+                style: isMobile
+                    ? TextStyles.extraBold28ptStix()
+                    : TextStyles.extraBold60ptStix(),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: margin),
+              Expanded(
+                child: ConstrainedBox(
+                  constraints: BoxConstraints(maxWidth: screenWidth * 0.9),
+                  child: Row(
+                    children: [
+                      Align(
+                          alignment: Alignment.topRight,
+                          child: _buildGif(Assets.gifCatFlower2, screenWidth * 0.1)),
+                      Expanded(child: WishWidget()),
+                      Align(
+                          alignment: Alignment.bottomLeft,
+                          child: _buildGif(Assets.gifCatFlower, screenWidth * 0.1)),
+                    ],
+                  ),
                 ),
               ),
-            ),
-            SizedBox(height: margin),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text("Prev Page"),
-            ),
-            SizedBox(height: margin),
-          ],
+              SizedBox(height: margin),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Prev Page"),
+              ),
+              SizedBox(height: margin),
+            ],
+          ),
         ),
       ),
     );
