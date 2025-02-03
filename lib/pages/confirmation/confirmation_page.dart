0fcd4e60e58fd89575a20da12787d5a3d9e6129f
Fakhry <fakhrymubarak@gmail.com>
2025-02-03 23:04:26 +0700

    fix confirmation page max width

--- lib/pages/confirmation/confirmation_page.dart
+++ lib/pages/confirmation/confirmation_page.dart
@@ -21,74 +21,78 @@ class ConfirmationPage extends StatelessWidget {
         child: Builder(builder: (context) {
           final viewModel = Provider.of<ConfirmationViewModel>(context);
           return Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: margin),
-                Text(
-                  "Next Page Belongs to \nShafa Only (  •̀ - •́  ) !!!",
-                  style: isMobile
-                      ? TextStyles.extraBold28ptStix()
-                      : TextStyles.extraBold60ptStix(),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: margin),
-                Text(
-                  "Proof that you are real Shafa( •̀⤙•́)",
-                  style: isMobile
-                      ? TextStyles.semiBold24pt()
-                      : TextStyles.semiBold16pt(),
-                ),
-                SizedBox(height: 16),
-                Text(
-                  "Since high school, what song has your boyfriend always reminded you of?",
-                  style: isMobile ? TextStyles.reg24pt() : TextStyles.reg16pt(),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 32),
-                  margin: const EdgeInsets.symmetric(vertical: 16),
-                  child: TextField(
-                    onChanged: (value) => viewModel.onAnswerChanged(value),
-                    decoration: InputDecoration(
-                      hintText: "Enter song name...",
-                      border: OutlineInputBorder(),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(maxWidth: 1400),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: margin),
+                  Text(
+                    "Next Page Belongs to \nShafa Only (  •̀ - •́  ) !!!",
+                    style: isMobile
+                        ? TextStyles.extraBold28ptStix()
+                        : TextStyles.extraBold60ptStix(),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: margin),
+                  Text(
+                    "Proof that you are real Shafa( •̀⤙•́)",
+                    style: isMobile
+                        ? TextStyles.semiBold24pt()
+                        : TextStyles.semiBold16pt(),
+                  ),
+                  SizedBox(height: 16),
+                  Text(
+                    "Since high school, what song has your boyfriend always reminded you of?",
+                    style: isMobile ? TextStyles.reg24pt() : TextStyles.reg16pt(),
+                    textAlign: TextAlign.center,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 32),
+                    margin: const EdgeInsets.symmetric(vertical: 16),
+                    child: TextField(
+                      onChanged: (value) => viewModel.onAnswerChanged(value),
+                      decoration: InputDecoration(
+                        hintText: "Enter song name...",
+                        border: OutlineInputBorder(),
+                      ),
+                      style:
+                          isMobile ? TextStyles.reg24pt() : TextStyles.reg16pt(),
                     ),
-                    style:
-                        isMobile ? TextStyles.reg24pt() : TextStyles.reg16pt(),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if (viewModel.checkHashes()) {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => WishPage()),
-                      );
-                    } else {
-                      showDialog(
-                        context: context,
-                        builder: (_) => AlertDialog(
-                          title: Text(viewModel.getErrorMessage()),
-                        ),
-                      );
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: Size(100, 60),
-                    backgroundColor: AppColors.yellow,
+                  ElevatedButton(
+                    onPressed: () {
+                      if (viewModel.checkHashes()) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => WishPage()),
+                        );
+                      } else {
+                        showDialog(
+                          context: context,
+                          builder: (_) => AlertDialog(
+                            title: Text(viewModel.getErrorMessage()),
+                          ),
+                        );
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: Size(100, 60),
+                      backgroundColor: AppColors.yellow,
+                    ),
+                    child: Text(
+                      "Submit Answer (୨୧ ❛ᴗ❛)✧",
+                      style: const TextStyle(color: Colors.black),
+                    ),
                   ),
-                  child: Text(
-                    "Submit Answer (୨୧ ❛ᴗ❛)✧",
-                    style: const TextStyle(color: Colors.black),
+                  SizedBox(height: margin),
+                  ElevatedButton(
+                    onPressed: () => Navigator.pop(context),
+                    child: Text("Prev Page (◞‸ ◟)💧"),
                   ),
-                ),
-                SizedBox(height: margin),
-                ElevatedButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: Text("Prev Page (◞‸ ◟)💧"),
-                ),
-                SizedBox(height: margin),
-              ],
+                  SizedBox(height: margin),
+                ],
+              ),
             ),
           );
         }),
