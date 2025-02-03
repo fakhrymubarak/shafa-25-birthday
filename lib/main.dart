737de48d4d155c54576022df4d7263ca5a7a86e4
Fakhry <fakhrymubarak@gmail.com>
2025-02-03 21:22:05 +0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:shafa_25_birthday/pages/home_page.dart';
 import 'package:shafa_25_birthday/themes/styles/app_themes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
